00cfe157a95b8ba719eeefcb0d6679d329e38160
a-a-k <a.a.krasnovsky@gmail.com>
2022-09-15 19:24:49 +0700

    first implementation

---
+++ RSAkeyGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+var rsa = RSA.Create(2048);
+var key = new RsaSecurityKey(rsa);
+var xml = key.Rsa.ToXmlString(true);
+var filename = !args.Any() || string.IsNullOrEmpty(args[0]) ? $"rsa-{DateTimeOffset.UtcNow.UtcTicks}.xml" : args[0];
+
+File.WriteAllText(filename, xml);
